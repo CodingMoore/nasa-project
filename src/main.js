e29d38553ebe20d3a3ee7c8998196ccb0a6702e5
Danielle Thompson <danithompson74@gmail.com>
2020-11-19 09:55:09 -0800

    Move over extra business logic to new page Co-authored-by: Glen Buck <glenbuck503@gmail.com> Co-authored-by: Randel Moore <randel.c.moore@gmail.com>

--- src/main.js
+++ src/main.js
@@ -25,17 +25,3 @@ $(document).ready(function () {
     apiServiceCall(searchTerm);
   });
 });
-
-for (let i = 0; i <10; i++) {
-  let rando1 = Math.floor(Math.random() * 100);
-  // $("#image-display").html(`<img src="${response.collection.items[rando1].links[0].href}">`);
-  console.log(rando1);
-}
-
-setInterval(function(){ 
-  for (let i = 0; i < 1; i++) {
-  let rando1 = Math.floor(Math.random() * 100);
-  // $("#image-display").html(`<img src="${response.collection.items[rando1].links[0].href}">`);
-  console.log(rando1);
-}; }, 5000);
-
